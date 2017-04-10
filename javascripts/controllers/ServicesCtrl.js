e3a3f2eb4bcb1aecba31bc105c35d2fe82cf7e4d
Debra Gordon <debra.m.gordon@gmail.com>
2017-04-10 12:40:00 -0500

    files altered to go live on dg tech site

--- javascripts/controllers/ServicesCtrl.js
+++ javascripts/controllers/ServicesCtrl.js
@@ -4,9 +4,9 @@ app.controller("ServicesCtrl", function ($scope, $routeParams, $rootScope, $http
 
   console.log("services is connected");
 
-  $http.get("/data/siteContent.json").then(response =>{
-    $scope.siteData = response.data;
-    $scope.testimonials = $scope.siteData.testimonials;
-  });
+  // $http.get("/data/siteContent.json").then(response =>{
+  //   $scope.siteData = response.data;
+  //   $scope.testimonials = $scope.siteData.testimonials;
+  // });
 
 });
