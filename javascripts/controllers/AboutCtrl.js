fb44a806d4e0052965f78e1478651aa168c9855f
Debra Gordon <debra.m.gordon@gmail.com>
2017-04-10 12:56:07 -0500

    updated necessary links to make it go live on dmgtech

--- javascripts/controllers/AboutCtrl.js
+++ javascripts/controllers/AboutCtrl.js
@@ -4,7 +4,7 @@ app.controller("AboutCtrl", function ($scope, $routeParams, $rootScope, $http){
 
   console.log("About is connected");
 
-  $http.get("/data/siteContent.json").then(response =>{
+  $http.get("http://www.debragordon.tech/FMND/data/siteContent.json").then(response =>{
     $scope.siteData = response.data;
     $scope.images = $scope.siteData.sliderImages;
   });
